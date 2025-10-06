664aebfbb3ecde80d0dfeeabd862d0c5af036c29
Mark Brown <mjbrown@microsoft.com>
2025-10-06 07:54:16 -0700

    updating branch to Sync

---
+++ frontend/.angular/cache/19.1.8/multi-agent-app/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,18 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-YDT55EBH.js";
+import "./chunk-I6NCUY32.js";
+import "./chunk-YBDCMBNM.js";
+import "./chunk-K2TGAMWK.js";
+import "./chunk-6Q4RANH6.js";
+import "./chunk-FFZIAYYX.js";
+import "./chunk-CXCX2JKZ.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
---
+++ frontend/.angular/cache/19.1.8/multi-agent-app/vite/deps/@angular_material_divider.js
@@ -0,0 +1,16 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-RYUET3OT.js";
+import "./chunk-R4SIYDRJ.js";
+import "./chunk-DINLEAYU.js";
+import "./chunk-I6NCUY32.js";
+import "./chunk-YBDCMBNM.js";
+import "./chunk-K2TGAMWK.js";
+import "./chunk-6Q4RANH6.js";
+import "./chunk-FFZIAYYX.js";
+import "./chunk-CXCX2JKZ.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
